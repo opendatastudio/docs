82aa35ff15e4645a21da0b2342b98da776465a4e
Varvara Efremova <varvara@echus.co>
2024-09-01 15:26:10 +1000

    Rename Github org to opendatastudio

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
         src: './src/assets/logo.svg',
       },
       social: {
-        github: "https://github.com/opendatafit/docs",
+        github: "https://github.com/opendatastudio/docs",
       },
       sidebar: [
         {
