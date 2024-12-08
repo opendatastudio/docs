cf508c4d1a929136685fc0b435a82418119f094d
James Wilmot <jameswilmot2000@gmail.com>
2024-12-08 16:19:23 +1100

    flake and .envrc

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
       devShells.default = pkgs.mkShell {
         buildInputs = [
           # Set the major version of Node.js
-          pkgs.nodejs-20_x
+          pkgs.nodejs_20
           pkgs.yarn
           pkgs.nodePackages.typescript
           # pkgs.nodePackages.typescript-language-server
